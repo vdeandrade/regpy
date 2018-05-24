65a2445d87999d5b5eac19c35ff9d4c8d1cf0cc6
Francesco De Carlo <decarlof@gmail.com>
2018-05-24 12:23:15 -0500

    added basic pyqt widgets

--- regpy/gui.py
+++ regpy/gui.py
@@ -1,3 +1,23 @@
+import os
+import sys
+import logging
+import pkg_resources
+import pyqtgraph as pg
+#import tifffile
+#import dxchange as dx
+#import ufot.widgets
+#import ufot.process
+#import ufot.util as util
+#import ufot.config as config
+#import ufot.reco as reco
+
+from argparse import ArgumentParser
+import numpy as np
+from contextlib import contextmanager
+from PyQt4 import QtGui, QtCore, uic
+
+
+
 import tomopy
 import dxchange
 import numpy as np
@@ -7,41 +27,44 @@ import h5py
 from skimage.feature import register_translation
 print('\n*** Libraries imported')
 
-
-##########################################################################################################
-fdir = '/local/data/2018-04/Dubacq/'
-file_name_Im1 = 'tomo_manip7G-sc_7124eV_1200prj_354.h5'
-file_name_Im2 = 'tomo_manip7G-sc_7200eV_1200prj_352.h5'
-prj = 0
-binning = 1
-medfilt_size = 3
-crop = [200,300]
-##########################################################################################################
-
-# Reading Images to align:
-Im1, flat1, dark1, theta = dxchange.read_aps_32id(fdir+file_name_Im1, proj=(prj, prj+1, 1))
-Im2, flat2, dark2, theta = dxchange.read_aps_32id(fdir+file_name_Im2, proj=(prj, prj+1, 1))
-
-Im1 = Im1/np.mean(flat1, axis = 0)
-Im2 = Im2/np.mean(flat2, axis = 0)
-
-if medfilt_size>0:
-    Im1 = ndimage.median_filter(Im1,footprint=np.ones((1, medfilt_size, medfilt_size)))
-    Im2 = ndimage.median_filter(Im2,footprint=np.ones((1, medfilt_size, medfilt_size)))
-
-if binning>0:
-    Im1 = tomopy.downsample(Im1, level=binning)
-    Im1 = tomopy.downsample(Im1, level=binning, axis=1)
-
-    Im2 = tomopy.downsample(Im2, level=binning)
-    Im2 = tomopy.downsample(Im2, level=binning, axis=1)
+LOG = logging.getLogger(__name__)
 
 
-if 1:
-    plt.figure(),
-    plt.subplot(1,2,1), plt.imshow(np.squeeze(Im1), cmap='gray', aspect="auto", interpolation='none'), plt.colorbar()
-    plt.subplot(1,2,2), plt.imshow(np.squeeze(Im2), cmap='gray', aspect="auto", interpolation='none'), plt.colorbar()
-    plt.show()
+def main_call():
+    ##########################################################################################################
+    fdir = '/local/data/2018-04/Dubacq/'
+    file_name_Im1 = 'tomo_manip7G-sc_7124eV_1200prj_354.h5'
+    file_name_Im2 = 'tomo_manip7G-sc_7200eV_1200prj_352.h5'
+    prj = 0
+    binning = 1
+    medfilt_size = 3
+    crop = [200,300]
+    ##########################################################################################################
+
+    # Reading Images to align:
+    Im1, flat1, dark1, theta = dxchange.read_aps_32id(fdir+file_name_Im1, proj=(prj, prj+1, 1))
+    Im2, flat2, dark2, theta = dxchange.read_aps_32id(fdir+file_name_Im2, proj=(prj, prj+1, 1))
+
+    Im1 = Im1/np.mean(flat1, axis = 0)
+    Im2 = Im2/np.mean(flat2, axis = 0)
+
+    if medfilt_size>0:
+        Im1 = ndimage.median_filter(Im1,footprint=np.ones((1, medfilt_size, medfilt_size)))
+        Im2 = ndimage.median_filter(Im2,footprint=np.ones((1, medfilt_size, medfilt_size)))
+
+    if binning>0:
+        Im1 = tomopy.downsample(Im1, level=binning)
+        Im1 = tomopy.downsample(Im1, level=binning, axis=1)
+
+        Im2 = tomopy.downsample(Im2, level=binning)
+        Im2 = tomopy.downsample(Im2, level=binning, axis=1)
+
+
+    if 1:
+        plt.figure(),
+        plt.subplot(1,2,1), plt.imshow(np.squeeze(Im1), cmap='gray', aspect="auto", interpolation='none'), plt.colorbar()
+        plt.subplot(1,2,2), plt.imshow(np.squeeze(Im2), cmap='gray', aspect="auto", interpolation='none'), plt.colorbar()
+        plt.show()
 
 
 def transform_image(img, rotation=0, translation=(0, 0), crop=False):
@@ -98,3 +121,107 @@ def transform_image(img, rotation=0, translation=(0, 0), crop=False):
         out = out[crops[0]:crops[1],
                   crops[2]:crops[3]]
     return out, crops
+
+def remove_extrema(data):
+    upper = np.percentile(data, 99)
+    lower = np.percentile(data, 1)
+    data[data > upper] = upper
+    data[data < lower] = lower
+    return data
+
+class OverlapViewer(QtGui.QWidget):
+    """
+    Presents two images by subtracting the flipped second from the first.
+
+    To get the current deviation connect to the *slider* attribute's
+    valueChanged signal.
+    """
+    def __init__(self, parent=None):
+        super(OverlapViewer, self).__init__()
+        image_view = pg.ImageView()
+        image_view.getView().setAspectLocked(True)
+        self.image_item = image_view.getImageItem()
+
+        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider.setRange(0, 0)
+        self.slider.valueChanged.connect(self.update_image)
+
+        self.main_layout = QtGui.QVBoxLayout()
+        self.main_layout.addWidget(image_view)
+        self.main_layout.addWidget(self.slider)
+        self.setLayout(self.main_layout)
+        self.first, self.second = (None, None)
+
+    def set_images(self, first, second):
+        """Set *first* and *second* image."""
+        self.first = remove_extrema(first.T)
+        self.second = remove_extrema(np.flipud(second.T))
+
+        if self.first.shape != self.second.shape:
+            LOG.warn("Shape {} of {} is different to {} of {}".
+                     format(self.first.shape, self.first, self.second.shape, self.second))
+
+        self.slider.setRange(0, self.first.shape[0])
+        self.slider.setSliderPosition(self.first.shape[0] / 2)
+        self.update_image()
+
+    def set_position(self, position):
+        self.slider.setValue(int(position))
+        self.update_image()
+
+    def update_image(self):
+        """Update the current subtraction."""
+        if self.first is None or self.second is None:
+            LOG.warn("No images set yet")
+        else:
+            pos = self.slider.value()
+            moved = np.roll(self.second, self.second.shape[0] / 2 - pos, axis=0)
+            self.image_item.setImage(moved - self.first)
+
+    
+class ApplicationWindow(QtGui.QMainWindow):
+    def __init__(self, app):
+        QtGui.QMainWindow.__init__(self)
+        self.app = app
+        ui_file = pkg_resources.resource_filename(__name__, 'gui.ui')
+        self.ui = uic.loadUi(ui_file, self)
+        self.ui.show()
+    
+        # set up run-time widgets
+        self.overlap_viewer = OverlapViewer()
+        self.ui.overlap_layout.addWidget(self.overlap_viewer)
+
+        # connect signals
+        #self.overlap_viewer.slider.valueChanged.connect(self.center_slider_changed)
+        self.ui.arrow_right.clicked.connect(self.arrow_right_clicked)
+        self.ui.arrow_left.clicked.connect(self.arrow_left_clicked)
+        self.ui.arrow_up.clicked.connect(self.arrow_up_clicked)
+        self.ui.arrow_down.clicked.connect(self.arrow_down_clicked)
+        self.ui.rotate_clock.clicked.connect(self.rotate_clock_clicked)
+        self.ui.rotate_cclock.clicked.connect(self.rotate_cclock_clicked)
+
+    def arrow_right_clicked(self, checked):
+        print("clicking right")
+
+    def arrow_left_clicked(self, checked):
+        print("clicking left")
+
+    def arrow_up_clicked(self, checked):
+        print("clicking up")
+
+    def arrow_down_clicked(self, checked):
+        print("clicking down")
+
+    def rotate_clock_clicked(self, checked):
+        print("rotating clock")
+
+    def rotate_cclock_clicked(self, checked):
+        print("rotating counter clock")
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ApplicationWindow(app)
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
